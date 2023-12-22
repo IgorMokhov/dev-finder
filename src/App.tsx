3d2c163c7dcd0fef0ffb4ba04c9b59db6305476e
Igor Mokhov <igormokhovid@gmail.com>
2023-12-22 18:28:19 +0300

    Added Search and Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Container } from "components/Container";
-import { TheHeader } from "components/TheHeader";
-
+import { useState } from 'react';
+import { Container } from 'components/Container';
+import { Search } from 'components/Search';
+import { TheHeader } from 'components/TheHeader';
 
 function App() {
+  const [hasError, setError] = useState(true);
+
+  const onSubmit = (text: string) => {};
+
   return (
     <Container>
       <TheHeader />
+      <Search hasError={hasError} onSubmit={onSubmit} />
     </Container>
   );
 }
